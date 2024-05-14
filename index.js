e578f51023088a3b43c5b70e6005b2b45689f878
khalyl rocha <khalylfreitas@gmail.com>
2024-05-14 14:38:42 +0000

    Criação do comando /oi

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/oi', function (req, res) {
+  res.send('Olá Mundo!')
 })
+
+//criar uma rota [GET] /oi que retorna ola mundo
 
 app.listen(3000)
